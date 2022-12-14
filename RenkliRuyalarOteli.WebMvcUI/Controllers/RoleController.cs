97984ae926373b424fa203e89b825a2662699376
edanurktly <kutluayedanur@gmail.com>
2022-12-14 20:15:38 +0300

    role

---
+++ RenkliRuyalarOteli.WebMvcUI/Controllers/RoleController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RenkliRuyalarOteli.BL.Abstract;
+
+namespace RenkliRuyalarOteli.WebMvcUI.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly IRoleManager roleManager;
+        public RoleController(IRoleManager roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await roleManager.FindAllAsync();
+            return View(result);
+        }
+    }
+}

41e8828d7360dea979db5444d157bc3cee74673f
edanurktly <kutluayedanur@gmail.com>
2022-12-15 19:07:26 +0300

    Admin Layout Gelistirildi...

--- RenkliRuyalarOteli.WebMvcUI/Program.cs
+++ RenkliRuyalarOteli.WebMvcUI/Program.cs
@@ -37,6 +37,15 @@ namespace RenkliRuyalarOteli.WebMvcUI
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+
+
             app.Run();
         }
     }
